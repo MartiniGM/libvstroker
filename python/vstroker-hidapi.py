f65dd48d223560e35a5be39b73aa564a77be0826
Kyle Machulis <kyle@nonpolynomial.com>
2012-11-25 15:14:19 -0800

    More hidapi lib changes, fixed 16-bit signed values

--- python/vstroker-hidapi.py
+++ python/vstroker-hidapi.py
@@ -1,5 +1,6 @@
 import sys
 import hid
+import time
 
 class VStrokerDevice(object):
     VID = 0x0451
@@ -9,49 +10,68 @@ class VStrokerDevice(object):
     def __init__(self):
         self._device = None
 
-    def getDeviceList(self):
+    @staticmethod
+    def getDeviceList():
+        devices = []
         for d in hid.enumerate(VStrokerDevice.VID, VStrokerDevice.PID):
-            keys = d.keys()
-            keys.sort()
-            for key in keys:
-                print "%s : %s" % (key, d[key])
-            print ""
+            # The device actually uses TI's VID, meaning the 55a5 PID is
+            # probably for their CC2500 dongle. Therefore, make sure we have
+            # check the product string is set, so we don't just access any ol'
+            # CC2500 dongle. Once I actually /have/ a CC2500 dongle this may
+            # change.
+            if d["product_string"] != "Vstroker":
+                continue
+            devices.append(d["path"])
+        return devices
 
-    def open(self, ):
+    def open(self, path):
         self._device = hid.device()
-        self._device.open(0x451, 0x55a5)
+        self._device.open_path(path)
 
     def readLoop(self):
-        print "Opening device"
-        h = hid.device(0x451, 0x55a5)
-        #h = hid.device(0x1941, 0x8021) # Fine Offset USB Weather Station
+        self._device.set_nonblocking(1)
 
-        print "Manufacturer: %s" % h.get_manufacturer_string()
-        print "Product: %s" % h.get_product_string()
-        print "Serial No: %s" % h.get_serial_number_string()
-        h.set_nonblocking(1)
-
-        # self.hidraw = open("/dev/hidraw4")
         while True:
-            try:
-                data = h.read(10)
-                if len(data) == 0:
-                    continue
-                axis = []
-                xor_byte = data[0]
-                for i in range(3):
-                    a = (((data[(i*2)+1] & 0xf) << 4) | (data[(i*2)+1] >> 4)) ^ xor_byte
-                    b = (((data[(i*2)+2] & 0xf) << 4) | (data[(i*2)+2] >> 4)) ^ xor_byte
-                    axis.append(a | b << 8)
-                print "%s" % (axis)
-            except KeyboardInterrupt:
-                break
+            data = self._device.read(10)
+            if len(data) == 0:
+                yield None
+                continue
+            axis = []
+            xor_byte = data[0]
+            for i in range(3):
+                a = (((data[(i*2)+1] & 0xf) << 4) | (data[(i*2)+1] >> 4)) ^ xor_byte
+                b = (((data[(i*2)+2] & 0xf) << 4) | (data[(i*2)+2] >> 4)) ^ xor_byte
+                c = a | (b << 8)
+                # convert to signed 16-bit int
+                if c > 2**15:
+                    c = c - 2**16
+                axis.append(c)
+            yield axis
 
 def main():
+    d = VStrokerDevice.getDeviceList()
+    if len(d) == 0:
+        print "No devices found!"
+        return 1
     v = VStrokerDevice()
-    v.getDeviceList()
-    v.open()
-    #v.readLoop()
+    v.open(d[0])
+    g = v.readLoop()
+    try:
+        c = 0
+        s = time.time()
+        while True:
+            l = g.next()            
+            if l is None:
+                time.sleep(.004)
+                continue
+            c = c + 1
+            e = time.time()
+            if e - s > 1.0:
+                print c
+                c = 0
+                s = time.time()
+    except KeyboardInterrupt:
+        return 0
 
 if __name__ == "__main__":
     sys.exit(main())
